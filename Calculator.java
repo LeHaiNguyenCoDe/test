1f75e88711861456f89ded2d1dae2a50f90a3d43
Nacht <ducvt1310@gmail.com>
2022-08-03 11:11:02 +0700

    Create project

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class public static void main(String[] args) {
+    int a, b, c;
+    c = Calculator.Add(a, b);
+}
+
+class Calculator {
+    public Add(int a, int b){
+        return a + b;
+    }
+}
